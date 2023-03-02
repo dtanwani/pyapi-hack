39f5618cf9e365ab631a9b5f37c84544baa64b04
dtanwani <dtanwani@users.noreply.github.com>
2023-03-02 17:09:47 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="dtanwani"
+original_name="pyapi_hack"
+original_urlname="pyapi-hack"
+original_description="Awesome pyapi_hack created by dtanwani"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv pyapi_hack $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
--- tests/test_base.py
+++ tests/test_base.py
@@ -1,5 +1,5 @@
-from project_name.base import NAME
+from pyapi_hack.base import NAME
 
 
 def test_base():
-    assert NAME == "project_name"
+    assert NAME == "pyapi_hack"
